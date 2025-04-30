bddc0b57aef09fe52f8b737b17da8a038301933f
Khoi Nguyen <akhoi90@gmail.com>
2025-04-29 23:00:42 -0400

    Add Windows build script

---
+++ build.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set BUILD_FLAGS=-g
+set BUILD_TYPE=_debug
+
+for %%i in (%*) do (
+    if "%%i"=="release" (
+        set BUILD_FLAGS=-O3
+        set BUILD_TYPE=
+    ) else if "%%i"=="loggable" (
+        set BUILD_FLAGS=!BUILD_FLAGS! -DLOGGABLE
+    )
+)
+
+set OUTPUT_FILE=build\bstbox%BUILD_TYPE%_windows.exe
+
+if not exist build mkdir build
+
+g++ -std=c++11 %BUILD_FLAGS% bstbox\source\*.cpp tree\source\*.cpp log\source\*.cpp -o %OUTPUT_FILE%
+
+if %errorlevel% neq 0 (
+    echo Build failed!
+    exit /b %errorlevel%
+) else (
+    echo Build succeeded! Output: %OUTPUT_FILE%
+)
--- build.sh
+++ build.sh
@@ -1,15 +1,33 @@
 OS_TYPE=$(uname | tr '[:upper:]' '[:lower:]')
-if [ "$1" == "release" ]; then
-    BUILD_FLAGS="-O3"
-    BUILD_TYPE=""
-else
-    BUILD_FLAGS="-g -DLOGGABLE"
-    BUILD_TYPE="_debug"
-fi
+
+BUILD_FLAGS="-g"
+BUILD_TYPE="_debug"
+for arg in "$@"; do
+    case $arg in
+        release)
+            BUILD_FLAGS="-O3"
+            BUILD_TYPE=""
+            ;;
+        loggable)
+            BUILD_FLAGS="$BUILD_FLAGS -DLOGGABLE"
+            ;;
+    esac
+done
+
 OUTPUT_FILE="build/bstbox${BUILD_TYPE}_${OS_TYPE}"
 
-mkdir build
+if [[ ! -d build ]]; then
+    mkdir build
+fi
+
 g++ -std=c++11 \
 $BUILD_FLAGS \
 bstbox/source/*.cpp tree/source/*.cpp log/source/*.cpp \
 -o $OUTPUT_FILE
+
+if [[ $? -eq 0 ]]; then
+    echo "Build succeeded. Output file: $OUTPUT_FILE"
+else
+    echo "Build failed."
+    exit 1
+fi
